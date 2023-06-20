05094bf8d62a0765816f7066016737e068d4762f
Wolves <cstingl@protonmail.com>
2023-06-20 19:59:19 -0400

    wip: Add parsing

--- src/contents.rs
+++ src/contents.rs
@@ -27,6 +27,56 @@ impl File {
             sentences: Vec::new(),
         }
     }
+
+    pub fn parse(&mut self) {
+        let mut contents = Vec::new();
+        let mut state = FileState::None;
+        let mut sentence = String::new();
+
+        for line in self.contents.lines() {
+            match state {
+                FileState::None => {
+                    if line.starts_with("```") {
+                        state = FileState::CodeBlock;
+                        sentence = String::new();
+                        sentence.push_str(line);
+                        sentence.push('\n');
+                    } else if line.starts_with("---") {
+                        state = FileState::Meta;
+                    } else if !line.starts_with('#') && !line.is_empty() {
+                        state = FileState::Sentence;
+                        sentence = String::new();
+                        sentence.push_str(line);
+                        sentence.push('\n');
+                    }
+                }
+                FileState::CodeBlock => {
+                    sentence.push_str(line);
+                    if line.starts_with("```") {
+                        contents.push(sentence);
+                        sentence = String::new();
+                        state = FileState::None;
+                    }
+                }
+                FileState::Meta => {
+                    if line.starts_with("---") {
+                        state = FileState::None;
+                    }
+                }
+                FileState::Sentence => {
+                    if line.is_empty() {
+                        state = FileState::None;
+                        contents.push(sentence);
+                        sentence = String::new();
+                    } else {
+                        sentence.push_str(line);
+                        sentence.push('\n');
+                    }
+                }
+            }
+        }
+        self.sentences = contents;
+    }
 }
 
 fn has_file_extension(path: &PathBuf, ending: &str) -> bool {
